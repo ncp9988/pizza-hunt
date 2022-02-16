56782862bb5696c8fb0ea4c6e6c95aa0fff5d36b
Chau Pham <ncp9988@gmail.com>
2022-02-16 22:31:57 +0700

    able to get all or one pizza

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,34 @@
+const { Pizaa } = require('../models');
+const Pizza = require('../models/Pizza');
+
+const pizzaController = {
+// get all pizzas
+getAllPizza(req, res) {
+    Pizza.find({})
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+// get one pizza by id
+getPizzaById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .then(dbPizzaData => {
+        // If no pizza is found, send 404
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+};
+
+module.export = pizzaController;
