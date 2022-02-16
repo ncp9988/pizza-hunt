e69c4f44bef593086f40289584734e8eda6d076f
Chau Pham <ncp9988@gmail.com>
2022-02-17 06:41:09 +0700

    associate pizza and comments models

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,24 +1,42 @@
 const { Schema, model, SchemaTypes } = require('mongoose');
 
 const PizzaSchema = new Schema({
-    pizzaName: {
-      type: String
+  pizzaName: {
+    type: String
+  },
+  createdBy: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  toppings: [],
+  ccomments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Comment'
+    }
+  ]
+},
+  {
+    toJSON: {
+      virtuals: true,
     },
-    createdBy: {
-      type: String
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now
-    },
-    size: {
-      type: String,
-      default: 'Large'
-    },
-    toppings: []
+    id: false
   });
-  
-  // create the Pizza model using the PizzaSchema
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+  return this.comments.length;
+});
+
+
+// create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
 // export the Pizza model
