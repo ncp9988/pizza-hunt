ec4d353eee02baa167e6b860b841f786fd30c3c6
Chau Pham <ncp9988@gmail.com>
2022-02-16 22:43:13 +0700

    able to update a pizza

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,3 +1,4 @@
+const res = require('express/lib/response');
 const { Pizaa } = require('../models');
 const Pizza = require('../models/Pizza');
 
@@ -36,6 +37,16 @@ createPizza({ body }, res) {
       .catch(err => res.status(400).json(err));
   },
 
+// update pizza by id
+updatePizza({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+  },
 };
-
 module.export = pizzaController;
