0cc397d4ad4a37683301f7ec74b33c534373ac4b
Chau Pham <ncp9988@gmail.com>
2022-02-16 22:12:47 +0700

    model created and exported

--- models/Pizza.js
+++ models/Pizza.js
@@ -18,3 +18,8 @@ const PizzaSchema = new Schema({
     toppings: []
   });
   
+  // create the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the Pizza model
+module.exports = Pizza;
